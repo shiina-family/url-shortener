bcb0a63f4ecb41359feee76740a99a1f61978bb8
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-19 07:27:59 +0900

    use typescript

---
+++ database.ts
@@ -0,0 +1,36 @@
+import sqlite3 from 'sqlite3';
+
+export class Database {
+  db;
+
+  constructor(path: string) {
+    this.db = new sqlite3.Database(path);
+    this.db.exec(`
+         CREATE TABLE IF NOT EXISTS redirectsData (
+             shorten_slug text NOT NULL UNIQUE,
+             target_url text NOT NULL
+         )
+     `);
+  }
+
+  async fetch(shorten_slug: string) {
+    const sql = `SELECT shorten_slug, target_url
+                   FROM redirectsData
+                  WHERE shorten_slug = ?`;
+    return await new Promise<any>((resolve, reject) => {
+      this.db.get(sql, [shorten_slug], (err, row) => {
+        if (err !== null) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+
+  post(shorten_slug: string, target_url: string) {
+    const sql = `INSERT INTO redirectsData
+                      VALUES (?, ?)`;
+    this.db.run(sql, [shorten_slug, target_url]);
+  }
+}
---
+++ index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import {Database} from './database';
+
+const app = express();
+const urls = new Database('urls.db');
+
+app.get('/:id', async (req, res) => {
+  const url = await urls.fetch(req.params.id);
+  const to = url ? url.target_url : 'https://google.com';
+  return res.redirect(to);
+});
+
+app.post('/register', async (req, res) => {
+  const slug = req.query.slug as string;
+  const target = req.query.target as string;
+  urls.post(slug, target);
+});
+
+app.use(express.static('static'));
+app.listen(8080);
