2a585a59fb03b05afd86b17b46199d2e336b4ed5
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-19 08:35:41 +0900

    redo

--- index.ts
+++ index.ts
@@ -5,18 +5,10 @@ const app = express();
 const urls = new Database('urls.db');
 
 app.get('/:id', async (req, res, next) => {
-  if (req.params.id == 'script.js' || req.params.id === "helloworld"){
-    next()
-    return;
-  }
   const url = await urls.fetch(req.params.id);
   const to = url ? url.target_url : 'https://google.com';
   return res.redirect(to);
 });
-
-app.get("/helloworld", async(_, res) => {
-  res.status(200).send('Hello!World')
-})
 
 app.post('/register', async (req, res) => {
   const slug = req.query.slug as string;
