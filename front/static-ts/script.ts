d26e92e9e067d92d3a1ee8d00e50808fa4d39ef3
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-23 16:33:48 +0900

    format

--- front/static-ts/script.ts
+++ front/static-ts/script.ts
@@ -2,10 +2,10 @@ const postUrl = async () => {
   const data = new FormData(document.getElementById("url-shortener-form") as HTMLFormElement);
   const slug = data.get('shorten_slug') as string;
   const target = data.get('target_url') as string;
-  const params = {'slug': slug, 'target': target}
+  const params = { 'slug': slug, 'target': target }
   const qs = new URLSearchParams(params);
 
-  fetch(`/register?${qs}`, {method: "POST"})
+  fetch(`/register?${qs}`, { method: "POST" })
     .then(async response => {
       const responseText = await response.text();
       if (response.status === 201) {
